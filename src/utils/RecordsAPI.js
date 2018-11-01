70ec7d91ec33f7059731ae113f84e9290c359a88
wangqi <wangqi2014105@163.com>
2018-11-01 22:39:14 +0800

    [feature] finish api

--- src/utils/RecordsAPI.js
+++ src/utils/RecordsAPI.js
@@ -1,1 +1,5 @@
-export const api = process.env.REACT_APP_RECORDS_API_URL || "http://localhost:5000";
+import axios from 'axios'
+const api = process.env.REACT_APP_RECORDS_API_URL || "https://5bd5a4609325280013d28890.mockapi.io";
+
+export const getAll = () => 
+    axios.get(`${api}/api/v1/records`)
