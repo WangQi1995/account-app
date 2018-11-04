ce7b1bb118ba619b6c99c65ce3c9ba0a431070e3
wangqi <wangqi2014105@163.com>
2018-11-04 13:12:38 +0800

    [feature] finish

--- src/components/Records.js
+++ src/components/Records.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Record from './Record'
 import * as RecordsAPI from '../utils/RecordsAPI'
+import RecordForm from './RecordForm'
+import AmountBox from './AmountBox'
 
 class Records extends Component {
   constructor() {
@@ -26,32 +28,108 @@ class Records extends Component {
     )
   }
 
+  addRecord(record) {
+    this.setState({
+      error: null,
+      isLoaded: true,
+      records: [
+        ...this.state.records,
+        record
+      ]
+    })
+  }
+
+  updateRecord(record, data) {
+    const recordIndex = this.state.records.indexOf(record)
+    const newRecords = this.state.records.map((item, index) => {
+      if(index !== recordIndex) {
+        return item
+      }
+
+      return {
+        ...item,
+        ...data
+      }
+    })
+    this.setState({
+      records: newRecords
+    })
+  }
+  
+  deleteRecord(record) {
+    const recordIndex = this.state.records.indexOf(record)
+    const newRecords = this.state.records.filter( (item, index) => index !== recordIndex)
+    this.setState({
+      records: newRecords
+    })
+  }
+
+  credits() {
+    let credits = this.state.records.filter( (record) => {
+      return record.amount >= 0
+    })
+
+    return credits.reduce((prev, curr) => {
+      return prev + Number.parseInt(curr.amount, 0)
+    }, 0)
+  }
+
+  debits() {
+    let credits = this.state.records.filter( (record) => {
+      return record.amount < 0
+    })
+
+    return credits.reduce((prev, curr) => {
+      return prev + Number.parseInt(curr.amount, 0)
+    }, 0)
+  }
+
+  balance() {
+    return this.credits() + this.debits()
+  }
   render() {
     const { error, isLoaded, records } = this.state
+    let recordsComponent
 
     if(error) {
-      return <div>Error: {error.message}</div>}
+      recordsComponent = <div>Error: {error.message}</div>}
       else if (!isLoaded) {
-        return <div>Loading</div>
+        recordsComponent = <div>Loading</div>
       }else {
-        return (
-          <div>
-            <h2>Records</h2>
-            <table className="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Title</th>
-                  <th>Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                {records.map((record) => <Record key={record.id} {...record}/>)}
-              </tbody>
-            </table>
-          </div>
+        recordsComponent = (
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Title</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {records.map((record) => (
+                <Record 
+                key={record.id} 
+                record={record} 
+                handleEditRecord={this.updateRecord.bind(this)}
+                handleDeleteRecord = {this.deleteRecord.bind(this)}/>
+              ))}
+            </tbody>
+          </table>
         );
     }
+
+    return (
+          <div>
+            <h2>Records</h2>
+            <div className="row mb-3">
+              <AmountBox text="Credits" type="success" amount={this.credits()}/>
+              <AmountBox text="Debits" type="danger" amount={this.debits()}/>
+              <AmountBox text="Balance" type="info" amount={this.balance()}/>
+            </div>
+            <RecordForm handleNewRecord={this.addRecord.bind(this)}/>
+            {recordsComponent}
+          </div>
+    )
   }
 }
 
--- src/utils/RecordsAPI.js
+++ src/utils/RecordsAPI.js
@@ -3,3 +3,12 @@ const api = process.env.REACT_APP_RECORDS_API_URL || "https://5bd5a4609325280013
 
 export const getAll = () => 
     axios.get(`${api}/api/v1/records`)
+
+export const create = (body) => 
+    axios.post(`${api}/api/v1/records`, body)
+
+export const update = (id, body) =>
+    axios.put(`${api}/api/v1/records/${id}`, body)
+
+export const remove = (id) =>
+    axios.delete(`${api}/api/v1/records/${id}`)
